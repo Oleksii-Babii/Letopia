cdf384b733ba94d30a20d15d43fea38cb8adc81a
Ihor Tryndey <igortryndey@gmail.com>
2024-04-28 00:14:59 +0100

    contact_us_manager.php implemntation

--- contact_us_manage.php
+++ contact_us_manage.php
@@ -1,26 +1,67 @@
-<?php
-    // Connect to the database
-    // Check if we are on the live server or a local XAMPP environment
-    if($_SERVER['SERVER_NAME'] == 'knuth.griffith.ie'){
-        // Path for the Knuth server
-        $path_to_mysql_connect = '../../../mysql_connect.php';
-    }else{
-        // Path for the local XAMPP server
-        $path_to_mysql_connect = 'mysql_connect.php';
-    }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css" media="screen">
+    
+    <title>Document</title>
 
-    // Require the mysql_connect.php file using the determined path
-    require_once $path_to_mysql_connect;
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <main>
+        <div id="table-container" >
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                  <th scope="col">id</th>
+                  <th scope="col">name</th>
+                  <th scope="col">email</th>
+                  <th scope="col">phone</th>
+                  <th scope="col">message</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <?php
+                    // Connect to the database
+                    // Check if we are on the live server or a local XAMPP environment
+                    if($_SERVER['SERVER_NAME'] == 'knuth.griffith.ie'){
+                        // Path for the Knuth server
+                        $path_to_mysql_connect = '../../../mysql_connect.php';
+                    }else{
+                        // Path for the local XAMPP server
+                        $path_to_mysql_connect = 'mysql_connect.php';
+                    }
+
+                    // Require the mysql_connect.php file using the determined path
+                    require_once $path_to_mysql_connect;
 
 
-    $stmt = ("SELECT * FROM contact_us");
+                    $stmt = ("SELECT * FROM contact_us");
 
-    $result = $db_connection->query($stmt);
+                    $result = $db_connection->query($stmt);
 
-    if($result->num_rows > 0){
+                    if($result->num_rows > 0){
 
-        while($row = $result -> fetch_assoc()){
-            echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
-        }
-    }
-?>
+                        while($row = $result -> fetch_assoc()){
+                            echo "
+                                <tr class='table-light' style='color:black;'>
+                                <th scope='row'>" . $row['id'] . "</th>
+                                <td>" . $row['name'] . "</td>
+                                <td>" . $row['email'] . "</td>
+                                <td>" . $row['phone'] . "</td>
+                                <td>" . $row['message'] . "</td>
+                                </tr>";
+                        }
+                    }
+                ?>
+              </tbody>
+          </table>
+        </div>
+    </main>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
